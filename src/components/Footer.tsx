79021db38a03fa72df05e042e0227ef2c89c4487
ravermame <funnybones.work@gmail.com>
2026-09-25 16:13:57 +0530

    fix: update phone number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
             <ul>
               <li>
                 <Phone size={18} className="contact-icon" />
-                <span>+91 98765 43210</span>
+                <span>+91 98960 85166</span>
               </li>
               <li>
                 <Mail size={18} className="contact-icon" />
